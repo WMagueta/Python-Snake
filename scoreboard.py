001a78634bd812a614af6f4c9659109094b94da1
Lucas Magueta <127806458+WMagueta@users.noreply.github.com>
2023-09-06 22:46:14 -0300

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -8,21 +8,28 @@ class Scoreboard(Turtle):
         self.goto(0, 180)
         self.color("orange")
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", "r") as data:
+            self.high_score = data.read()
+            self.high_score = int(self.high_score)
 
     def update(self):
         self.clear()
         self.goto(0, 175)
-        self.write(f"Score = {self.score}", False, "center", ("Verdana", 16, "normal"))
+        self.write(f"Score = {self.score}", False, "center", ("Verdana", 14, "normal"))
+        self.goto(0, 150)
+        self.write(f"High Score = {self.high_score}", False, "center", ("Verdana", 14, "normal"))
         
     def increase_score(self):
         self.score += 1
         self.update()
 
     def gameover(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        with open("data.txt", "r") as data:
+            if self.score > int(data.read()):
+                with open("data.txt", "w") as uptading_data:
+                    self.high_score = self.score
+                    uptading_data.write(str(self.high_score))
         self.score = 0
         self.update()
         self.goto(0, -27)
-        self.write(f"   Game Over\n High Score = {self.high_score}", False, "center", ("Verdana", 28, "normal"))
+        self.write(f"Game Over", False, "center", ("Verdana", 16, "normal"))
